29d9fc1a36ff092138a900a1ec70fbc5466f1add
03-Andrew <andreinadela052@gmail.com>
2024-03-01 17:55:09 +0800

    Update

--- PreciosoApp/Models/Types.cs
+++ PreciosoApp/Models/Types.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,105 @@ using System.Threading.Tasks;
 
 namespace PreciosoApp.Models
 {
-    internal class Types
+    public class TherapistTypes
     {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+
+        public List<TherapistTypes> GetTherapistTypes()
+        {
+            List<TherapistTypes> types = new List<TherapistTypes>();
+            Database db = new Database();
+
+            using (MySqlConnection conn = db.GetCon())
+            {
+                conn.Open();
+                string query = "select * from tbl_therapist_type";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            TherapistTypes type = new TherapistTypes();
+                            type.Id = reader.GetInt32("type_id");
+                            type.TypeName = reader.GetString("type");
+
+                            types.Add(type);
+                        }
+                    }
+                }
+                return types;
+            }
+        }
     }
 
     public class Gender
     {
         public int Id { get; set; }
-        public string Gender_name { get; set; }
+        public string GenderType { get; set; }
+
+        private static readonly Dictionary<string, int> Genders = new Dictionary<string, int>();
+        Database db = new Database();
+        public List<Gender> GetGenders()
+        {
+            List<Gender> types = new List<Gender>();
+
+
+            using (MySqlConnection conn = db.GetCon())
+            {
+                conn.Open();
+                string query = "select * from tbl_gender";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Gender type = new Gender();
+                            type.Id = reader.GetInt32("gender_id");
+                            type.GenderType = reader.GetString("gender");
+
+                            types.Add(type);
+                        }
+                    }
+                }
+                return types;
+            }
+        }
+
+    }
+    public class TherapistStatus
+    {
+        public int Id { get; set; }
+        public string StatusName { get; set; }
+        Database db = new Database();
+
+        public List<TherapistStatus> GetTStatus()
+        {
+            List<TherapistStatus> types = new List<TherapistStatus>();
+
+
+            using (MySqlConnection conn = db.GetCon())
+            {
+                conn.Open();
+                string query = "select * from tbl_therapist_status";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            TherapistStatus type = new TherapistStatus();
+                            type.Id = reader.GetInt32("status_id");
+                            type.StatusName = reader.GetString("status");
+
+                            types.Add(type);
+                        }
+                    }
+                }
+                return types;
+            }
+        }
     }
 }
--- PreciosoApp/ViewModels/TherapistViewModel.cs
+++ PreciosoApp/ViewModels/TherapistViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using PreciosoApp.Models;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PreciosoApp.ViewModels
 {
@@ -23,13 +25,191 @@ namespace PreciosoApp.ViewModels
             }
         }
 
+        public ObservableCollection<TherapistTypes> TherapistTypes { get; }
+        public ObservableCollection<Gender> Genders { get; }
+        public ObservableCollection<TherapistStatus> TherapistStatuses { get; }
+
+        public string NewTherapistName { get; set; }
+        public DateTime NewTherapistDOB { get; set; }
+        public string NewTherapistContactInfo { get; set; }
+        public string NewTherapistSched { get; set; }
+
+        private Therapist _selectedTherapist;
+        public Therapist SelectedTherapist
+        {
+            get => _selectedTherapist;
+            set
+            {
+                _selectedTherapist = value;
+                OnPropertyChanged(nameof(SelectedTherapist));
+            }
+        }
+
+        private Gender _selectedGender;
+        public Gender SelectedGender
+        {
+            get => _selectedGender;
+            set
+            {
+                _selectedGender = value;
+                OnPropertyChanged(nameof(SelectedGender));
+            }
+        }
+
+        private TherapistTypes _selectedTherapistType;
+        public TherapistTypes SelectedTherapistType
+        {
+            get => _selectedTherapistType;
+            set
+            {
+                _selectedTherapistType = value;
+                OnPropertyChanged(nameof(SelectedTherapistType));
+            }
+        }
+
+        private TherapistStatus _selectedTherapistStatus;
+        public TherapistStatus SelectedTherapistStatus
+        {
+            get => _selectedTherapistStatus;
+            set
+            {
+                _selectedTherapistStatus = value;
+                OnPropertyChanged(nameof(SelectedTherapistStatus));
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = SelectedTherapist.Name;
+                OnPropertyChanged(nameof(Name));
+
+            }
+        }
+
+        public ICommand UpdateCommand { get; private set; }
+        public ICommand AddTherapistCommand { get; }
+
         public TherapistViewModel()
         {
             var therapistInstance = new Therapist();
             allTherapist = new ObservableCollection<Therapist>(therapistInstance.GetAllTherapist());
             Therapist = allTherapist;
+
+            var therapistTypes = new TherapistTypes();
+            var genderTypes = new Gender();
+            var therapistStatus = new TherapistStatus();
+
+            TherapistTypes = new ObservableCollection<TherapistTypes>(therapistTypes.GetTherapistTypes());
+            Genders = new ObservableCollection<Gender>(genderTypes.GetGenders());
+            TherapistStatuses = new ObservableCollection<TherapistStatus>(therapistStatus.GetTStatus());
+
+            UpdateCommand = new RelayCommand(UpdateTherapist);
+            AddTherapistCommand = new RelayCommand(AddTherapist);
+
+            NewTherapistDOB = DateTime.Now;
+
         }
 
+        private void AddTherapist()
+        {
+
+            try
+            {
+                Therapist tp = new Therapist();
+                string name = NewTherapistName;
+                DateTime dob = NewTherapistDOB;
+                string contact = NewTherapistContactInfo;
+                string sched = NewTherapistSched;
+                int gender_id = SelectedGender.Id;
+                int status_id = SelectedTherapistStatus.Id;
+                int type_id = SelectedTherapistType.Id;
+
+                // Add the therapist to the database
+                tp.AddTherapist(name, dob.Date, contact, sched, gender_id, status_id, type_id);
+
+                // Refresh the Therapist collection
+                Therapist = new ObservableCollection<Therapist>(tp.GetAllTherapist());
+
+                // Clear out the fields
+                NewTherapistName = string.Empty;
+                NewTherapistDOB = DateTime.Now;
+                NewTherapistContactInfo = string.Empty;
+                NewTherapistSched = string.Empty;
+                SelectedGender = null;
+                SelectedTherapistStatus = null;
+                SelectedTherapistType = null;
+            }
+            catch (Exception ex)
+            {
+                // Display error message to the user
+
+
+
+            }
+        }
+
+
+
+        private void UpdateTherapist()
+        {
+            Therapist tp = new Therapist();
+            int id = SelectedTherapist.Id;
+            string name = SelectedTherapist.Name;
+            DateTime dob = SelectedTherapist.DOB;
+            string contact = SelectedTherapist.ContactInfo;
+            string sched = SelectedTherapist.Sched;
+            int gender_id = SelectedGender?.Id ?? GetGenderId(SelectedTherapist.Gender);
+            int status_id = SelectedTherapistStatus?.Id ?? GetStatusId(SelectedTherapist.Status);
+            int type_id = SelectedTherapistType?.Id ?? GetTypeId(SelectedTherapist.Type);
+  
+
+            tp.UpdateTherapist(id, name, dob, contact, sched, gender_id, status_id, type_id);
+
+            Therapist = new ObservableCollection<Therapist>(tp.GetAllTherapist());
+
+            OnPropertyChanged(nameof(SelectedTherapist));
+
+            SelectedGender = null;
+            SelectedTherapistType = null;
+            SelectedTherapistStatus = null;
+
+        }
+
+        public int GetGenderId(string gender)
+        {
+            foreach (var item in Genders)
+            {
+                if (item.GenderType == gender) return item.Id;
+            }
+            return -1;
+        }
+
+        public int GetStatusId(string status)
+        {
+            foreach (var item in TherapistStatuses)
+            {
+                if (item.StatusName == status)
+                {
+                    return item.Id;
+                }
+            }
+            return -1;
+        }
+        public int GetTypeId(string type)
+        {
+            foreach (var item in TherapistTypes)
+            {
+                if (item.TypeName == type)
+                {
+                    return item.Id;
+                }
+            }
+            return -1;
+        }
 
         public string SearchText
         {
@@ -56,3 +236,4 @@ namespace PreciosoApp.ViewModels
         }
     }
 }
+    
